c3bd80a227cf568508544213db88fe83abf2d549
Tone <bruno.scastro2012@hotmail.com>
2023-11-01 02:35:02 -0300

    feat: nova versão do BOT

--- passo_1/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load .env file
-load_dotenv()
-
-DISCORD_API_TOKEN = str(os.getenv("DISCORD_API_TOKEN"))
-
-commandPrefix = "!"
--- passo_2/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load .env file
-load_dotenv()
-
-JIRA_PROJECT_URL = str(os.getenv("JIRA_PROJECT_URL"))
-JIRA_USER_EMAIL = str(os.getenv("JIRA_USER_EMAIL"))
-JIRA_API_TOKEN = str(os.getenv("JIRA_API_TOKEN"))
